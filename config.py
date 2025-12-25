6944597b7b7dacff3e2fd8685fe934e9ad5294d4
Muaazbah1 <animajor204@gmail.com>
2025-12-26 00:32:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
 # إعدادات بوت تليجرام
-TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+TELEGRAM_BOT_TOKEN = "8308461865:AAG7viIjYF7C050AnkLebXQ5pasgzYSSdK0"
 # معرف القناة التي سيتم الاستماع إليها لتحميل ملفات PDF الجديدة
-TELEGRAM_CHANNEL_USERNAME = "@YOUR_CHANNEL_USERNAME_HERE" # مثال: @Aleppo_Med_Marks
+TELEGRAM_CHANNEL_USERNAME = "jjgradebot" # مثال: @Aleppo_Med_Marks
 
 # إعدادات قاعدة البيانات
 DB_NAME = "students_marks.db"
@@ -22,9 +22,9 @@ MARKS_PDF_PATH = "marks.pdf" # المسار الذي سيتم حفظ ملف ال
 # إعدادات التحليل الإحصائي
 # يجب استبداله بمعرف القناة (Channel ID) التي سيرسل إليها البوت النتائج الإحصائية
 # يجب أن يكون المعرف على شكل -100xxxxxxxxxx
-STATISTICS_OUTPUT_CHANNEL_ID = -100123456789 
+STATISTICS_OUTPUT_CHANNEL_ID = -1003659935810 
 STATISTICS_OUTPUT_FILE = "Statistics_Report.md"
 NORMAL_DISTRIBUTION_IMAGE = "Normal_Distribution.png"
 
 # قائمة بمعرفات المستخدمين (Telegram IDs) المسموح لهم بإرسال ملفات العلامات
-ADMIN_IDS = [123456789] # استبدل بمعرف التليجرام الخاص بك
+ADMIN_IDS = [1406058239] # استبدل بمعرف التليجرام الخاص بك
